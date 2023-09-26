5721e8fde2a27a60713dfc5cbfd6e01916caf133
jwlebert <j.lebert4402@gmail.com>
2023-09-25 22:46:12 -0400

    reorganized project and ran cargo fmt

--- the-book/chapter-2/c2-guessing-game/src/main.rs
+++ the-book/chapter-2/c2-guessing-game/src/main.rs
@@ -1,10 +1,10 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     println!("Guess the number!");
 
     loop {
